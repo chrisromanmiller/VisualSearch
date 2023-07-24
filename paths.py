575a86bfb6320ab25d0c51f5c1a3da67fd5a6ac5
Jeremy Shahan <shahanj@oregonstate.edu>
2023-07-24 16:46:43 -0500

    added paths.py

---
+++ paths.py
@@ -0,0 +1,9 @@
+styles_path = "./myntradataset/styles.csv"
+images_path = "./myntradataset/images/"
+
+
+image_tensor_path = "./tensors/image_tensor.pt"
+title_tensor_path = "./tensors/title_tensor.pt"
+
+
+sam_checkpoint ="./tensors/sam_vit_h_4b8939.pth"
